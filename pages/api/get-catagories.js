48c4f3b7e96b459add2589c8579d5b592fc6e053
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-08-03 00:35:05 +0600

    feat: api to get product catagories

---
+++ pages/api/get-catagories.js
@@ -0,0 +1,16 @@
+import api from ".";
+export default async function handler(req, res) {
+  const responseData = {
+    success: false,
+    products: [],
+  };
+  try {
+    const { data } = await api.get("products/categories");
+    responseData.success = true;
+    responseData.products = data;
+    res.json(responseData);
+  } catch (error) {
+    responseData.error = error.message;
+    res.status(500).json(responseData);
+  }
+}
