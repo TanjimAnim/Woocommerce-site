df60121497eeae0861614d33c7f1015ef3183238
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-08-03 00:33:11 +0600

    feat:wrapping component with contexts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ShoppingCartContextProvider } from "../context/CartContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ShoppingCartContextProvider>
+        <Component {...pageProps} />
+      </ShoppingCartContextProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
