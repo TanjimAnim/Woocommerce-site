67e86b1ecec29854439fdb2a4af244a3eab3f50e
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-09-25 12:56:21 +0600

    feat: removed breadcrumb

--- pages/product/[[...product_slug]].js
+++ pages/product/[[...product_slug]].js
@@ -11,9 +11,6 @@ const SingleProduct = ({ product, categories }) => {
     dispatch,
   } = useShoppingCartContext();
 
-  const router = useRouter();
-  const breadcrumb = router.components["/shop/[[...category]]"].resolvedAs;
-
   const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
@@ -23,18 +20,20 @@ const SingleProduct = ({ product, categories }) => {
   };
   return (
     <>
-      <Box
-        key={product.id}
-        marginX='130px'
-        marginTop='120px'
-        marginBottom='178px'
-      >
-        <Box display='flex' flexDirection='row' justifyContent='space-evenly'>
+      <Box key={product.id} marginX='130px' marginTop='120px' marginBottom='178px'>
+        <Box
+          display='flex'
+          flexDirection={{ base: "column-reverse", md: "row", lg: "row" }}
+          justifyContent='space-evenly'
+        >
           <Box>
             <Image src={product.images[0].src} width='580px' height='475px' />
           </Box>
-          <Box width='65%' padding='3rem' marginTop='24px'>
-            <Box>{breadcrumb}</Box>
+          <Box
+            width={{ base: "100%", md: "65%", lg: "65%" }}
+            padding={{ base: "1rem", md: "2rem", lg: "3rem" }}
+            marginTop='24px'
+          >
             <Box marginTop='24px'>
               <Text fontSize='16px' fontWeight={700}>
                 {product.name}
@@ -104,56 +103,60 @@ const SingleProduct = ({ product, categories }) => {
       </Box>
       <Box marginX='130px'>
         <Box>
-          <Text fontSize='3xl' fontWeight={700}>
+          <Text fontSize={{ base: "lg", md: "2xl", xl: "3xl" }} fontWeight={700}>
             Related Products
           </Text>
         </Box>
-        <Box display='flex' flexWrap='wrap'>
-          {categories.map((item) => {
-            return (
-              <Box key={item.id}>
-                {item.id !== product.id ? (
-                  <>
-                    <Box
-                      key={item.id}
-                      width={{ base: "84%", md: "85%", lg: "100%" }}
-                      marginLeft='10px'
-                      marginRight='10px'
-                      marginTop='5px'
-                      marginBottom='5px'
-                    >
-                      <Box>
-                        <Image src={item.images[0].src} width='280px' />
-                      </Box>
-                      <Box>
-                        <Link href={`/product/${item.slug}/${item.id}`}>
+        <Box display='flex' flexWrap='wrap' marginLeft='-9px'>
+          {categories
+            .filter((item, index) => {
+              return index < 2;
+            })
+            .map((item) => {
+              return (
+                <Box key={item.id}>
+                  {item.id !== product.id ? (
+                    <>
+                      <Box
+                        key={item.id}
+                        width={{ base: "84%", md: "85%", lg: "100%" }}
+                        marginLeft='10px'
+                        marginRight='10px'
+                        marginTop='5px'
+                        marginBottom='5px'
+                      >
+                        <Box>
+                          <Image src={item.images[0].src} width='280px' />
+                        </Box>
+                        <Box>
+                          <Link href={`/product/${item.slug}/${item.id}`}>
+                            <Text
+                              marginRight='10px'
+                              marginTop='10px'
+                              cursor='pointer'
+                              fontSize='16px'
+                              fontWeight='bold'
+                            >
+                              {item.name}
+                            </Text>
+                          </Link>
+                        </Box>
+                        <Box>
                           <Text
-                            marginRight='10px'
-                            marginTop='10px'
-                            cursor='pointer'
-                            fontSize='16px'
-                            fontWeight='bold'
-                          >
-                            {item.name}
-                          </Text>
-                        </Link>
+                            dangerouslySetInnerHTML={{
+                              __html: item.price_html,
+                            }}
+                            textDecoration='none'
+                          />
+                        </Box>
                       </Box>
-                      <Box>
-                        <Text
-                          dangerouslySetInnerHTML={{
-                            __html: item.price_html,
-                          }}
-                          textDecoration='none'
-                        />
-                      </Box>
-                    </Box>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Box>
-            );
-          })}
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              );
+            })}
         </Box>
       </Box>
     </>
@@ -162,8 +165,7 @@ const SingleProduct = ({ product, categories }) => {
 export default SingleProduct;
 
 export async function getServerSideProps(context) {
-  const productId =
-    context.params.product_slug[context.params.product_slug.length - 1];
+  const productId = context.params.product_slug[context.params.product_slug.length - 1];
   const { data: product } = await api.get(`products/${productId}`);
   const categoryId = product.categories[0].id;
   const { data: categories } = await api.get("products", {
